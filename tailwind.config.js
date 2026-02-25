512d8d647c4b09a785d77339cbc7709cb649a5be
oof2510 <64423517+OOF2510@users.noreply.github.com>
2026-02-24 23:47:04 -0500

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-const nativewind = require('nativewind/plugin');
-
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './index.js'],
-  plugins: [nativewind],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
